919964c197fcf5061c9901d23d1629a087f17918
Nazar Aziz <mcnazar@gmail.com>
2015-08-20 12:19:19 +0100

    paginate recipes index

--- src/services/data/recipesList.js
+++ src/services/data/recipesList.js
@@ -1,20 +1,60 @@
-import { Recipes } from 'models/recipe';
+import { Recipe, Recipes } from 'models/recipe';
 
 export default class {
 
+    constructor( options = {} ) {
+        this.limit = options.limit || 10;
+        this.offset = options.offset || this.limit * options.page;
+    }
+
     execute() {
-        return Recipes
-            .query( {
-                where: {
-                    visibility: 1
-                }
-            } )
-            .fetch( {
-                withRelated: [ 'user', 'oils' ]
-            } );
+        return getRecipesCount.call( this )
+            .bind( this )
+            .then( setCount )
+            .then( getRecipes )
+            .then( setRecipes )
+            .then( returnRecipes );
     }
 }
 
 //////////////////
 ////// Private
 
+function getRecipesCount() {
+    return Recipe
+        .query( qb => {
+            qb
+                .count( 'id' )
+                .where( { visibility: 1 } )
+        } )
+        .fetch();
+}
+
+function setCount( count ) {
+    this.count = count
+}
+
+function getRecipes() {
+    return Recipes
+        .query( qb => {
+            qb
+                .where( { visibility: 1 } )
+                .orderBy( 'created_at', 'desc' )
+                .offset( this.offset )
+                .limit( this.limit );
+        } )
+        .fetch( {
+            withRelated: [ 'user', 'oils' ]
+        } );
+}
+
+function setRecipes( recipes ) {
+    this.recipes = recipes;
+}
+
+function returnRecipes() {
+    return {
+        count: this.count.get( 'count' ),
+        recipes: this.recipes
+    }
+}
